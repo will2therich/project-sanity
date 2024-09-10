aef65cfbed35fb18b6258f73fcf58038fe3127db
William Rich <william.rich@weblabs.dev>
2024-09-10 22:21:57 +0000

    Add UserResource and Filament integration
    Introduced UserResource with CRUD pages for User model using Filament. Updated User model to implement FilamentUser contract, added migration method and access control for the admin panel.

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,11 +3,14 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUserphp
 {
     use HasFactory, Notifiable;
 
@@ -43,5 +46,37 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function migration(Blueprint $table) {
+        $table->id();
+        $table->string('name');
+        $table->string('email')->unique();
+        $table->timestamp('email_verified_at')->nullable();
+        $table->string('password');
+        $table->foreignId('parent_id')->nullable();
+        $table->boolean('administrator')->default(false);
+        $table->string('stripe_id')->nullable()->index();
+        $table->string('pm_type')->nullable();
+        $table->string('pm_last_four', 4)->nullable();
+        $table->timestamp('trial_ends_at')->nullable();
+        $table->json('custom_fields')->nullable();
+        $table->string('avatar_url')->nullable();
+
+        $table->rememberToken();
+        $table->timestamps();
+    }
+
+    public function canAccessPanel(Panel $panel): bool
+    {
+        $panelId = $panel->getId();
+
+        switch ($panelId) {
+            case 'admin':
+                if ($this->administrator) return true;
+                return false;
+            default:
+                return false;
+        }
     }
 }
