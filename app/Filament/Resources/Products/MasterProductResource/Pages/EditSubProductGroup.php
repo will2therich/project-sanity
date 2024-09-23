79cababb61f7b7efd0e76efb829ed612c4c0a733
William Rich <william.rich@weblabs.dev>
2024-09-23 02:27:28 +0000

    Add form schema and subRecord handling to EditSubProductGroup
    Integrated form schema for sub-product group details and products using Filament components. Enhanced the mount method to handle subRecord, improving the flexibility and functionality of the EditSubProductGroup page.

--- app/Filament/Resources/Products/MasterProductResource/Pages/EditSubProductGroup.php
+++ app/Filament/Resources/Products/MasterProductResource/Pages/EditSubProductGroup.php
@@ -4,12 +4,17 @@ namespace App\Filament\Resources\Products\MasterProductResource\Pages;
 
 use App\Filament\Resources\Products\MasterProductResource;
 use App\Helpers\Pages\ProductsPagesHelper;
+use Filament\Forms\Components\Repeater;
+use Filament\Forms\Components\Section;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Concerns\InteractsWithForms;
+use Filament\Forms\Form;
 use Filament\Resources\Pages\Concerns\InteractsWithRecord;
 use Filament\Resources\Pages\Page;
 
 class EditSubProductGroup extends Page
 {
-    use InteractsWithRecord;
+    use InteractsWithRecord, InteractsWithForms;
 
     protected static string $resource = MasterProductResource::class;
 
@@ -18,14 +23,30 @@ class EditSubProductGroup extends Page
     protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
     protected static bool $shouldRegisterNavigation = false;
 
-    public function mount(int | string $record): void
+    protected $subRecord;
+    public function mount(int | string $record, $subRecord): void
     {
+        $this->subRecord = \App\Models\Products\SubProductGroups::find($subRecord);
         $this->record = $this->resolveRecord($record);
     }
 
     public function getSubNavigation(): array
     {
         return ProductsPagesHelper::getSubNavigation($this->getRecord());
+    }
+
+    public function form(Form $form): Form
+    {
+        return $form->schema([
+           Section::make('Sub-Group Details')->schema([
+               TextInput::make('name'),
+           ]),
+            Section::make('Products')->schema([
+                Repeater::make('Products')
+                    ->grid()
+                    ->columns(2)
+            ])
+        ]);
     }
 
 }
